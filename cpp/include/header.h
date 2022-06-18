c37b246008ecf98f079f289f1efac660a259791d
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2022-06-18 23:59:41 +0900

    Fix Sum2 method

--- cpp/include/header.h
+++ cpp/include/header.h
@@ -12,6 +12,6 @@ std::int32_t DLLEXPORT Sum(std::int32_t a, std::int32_t b);
 
 using FnSum = std::int32_t (*)(std::int32_t, std::int32_t);
 
-std::int32_t DLLEXPORT Sum2(FnSum *fn, std::int32_t a, std::int32_t b);
+std::int32_t DLLEXPORT Sum2(FnSum fn, std::int32_t a, std::int32_t b);
 
 }
--- cpp/src/source.cpp
+++ cpp/src/source.cpp
@@ -1,10 +1,10 @@
 #include "header.h"
-
+#include <stdio.h>
 
 std::int32_t DLLEXPORT Sum(std::int32_t a, std::int32_t b) {
     return a + b;
 }
 
-std::int32_t DLLEXPORT Sum2(FnSum *fn, std::int32_t a, std::int32_t b) {
+std::int32_t DLLEXPORT Sum2(FnSum fn, std::int32_t a, std::int32_t b) {
     return fn != nullptr ? (*fn)(a, b) : 0;
 }
